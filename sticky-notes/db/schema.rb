9e360ec1a0c67ff47a0dbf14952c9798f55443f5
Henry Craig <h.craig52@yahoo.com>
2019-06-19 16:02:54 -0400

    adding boxes around sections

--- sticky-notes/db/schema.rb
+++ sticky-notes/db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2019_06_19_185435) do
 
   create_table "sections", force: :cascade do |t|
-    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
