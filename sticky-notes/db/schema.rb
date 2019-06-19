42b02d154efc2937b9e9701a8b4331e6ba755545
dasha_bo_or <sonja.rosenfeld@gmail.com>
2019-06-19 15:47:40 -0400

    title finally added

--- sticky-notes/db/schema.rb
+++ sticky-notes/db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_06_19_185435) do
 
   create_table "sections", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
