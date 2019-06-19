b4a92e0e3487c082af07ff2d1c169f6a8cdacc48
dasha_bo_or <sonja.rosenfeld@gmail.com>
2019-06-19 15:17:27 -0400

    title added

--- sticky-notes/app/controllers/sections_controller.rb
+++ sticky-notes/app/controllers/sections_controller.rb
@@ -69,6 +69,7 @@ class SectionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def section_params
-      params.fetch(:section, {})
+      # params.fetch(:section, {})
+      params.require(:section).permit(:title)
     end
 end
